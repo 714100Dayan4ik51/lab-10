6074449bd0eace520db2453817389f82437d5992
714100Dayan4ik51 <dayan4ik51@gmail.com>
2023-11-23 15:37:23 +0500

    создаем функцию для выбора случайного числа из диапазона

--- lab10.py
+++ lab10.py
@@ -3,3 +3,9 @@ import logging
 
 # Настройка логгирования
 logging.basicConfig(filename='guessing_game.log', level=logging.INFO, format = '%(asctime)s - %(message)s')
+
+
+def guess_number(N, k):
+    logging.info(f"Начинаем игру с  N={N} и k={k}")
+    secret_number = random.randint(1, N)
+    logging.info(f"Загаданное число: {secret_number}")
